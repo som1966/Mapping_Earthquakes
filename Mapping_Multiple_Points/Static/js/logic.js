6899b7d157489a103e45f043501ce14c48601bd3
nkcondon <schnooze@hotmail.com>
2021-11-28 16:56:03 -0500

    add files

--- Mapping_Multiple_Points/Static/js/logic.js
+++ Mapping_Multiple_Points/Static/js/logic.js
@@ -2,7 +2,18 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "red"
+}).addTo(map);
 
 //  Add a marker to the map for Los Angeles, California.
 //let marker = L.marker([34.0522, -118.2437]).addTo(map);
